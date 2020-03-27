73b0f67bdcebc4c6383c748c35a72fe9b6add6f3
Renato <renato@Renatos-iMac.local>
2020-03-27 14:32:33 -0300

    Mudança de validação do backend

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { celebrate, Segments, Joi } = require('celebrate');
 
 const OngController = require('./controllers/OngController')
 const IncidentController = require('./controllers/IncidentController')
@@ -10,13 +11,35 @@ const routes = express.Router();
 routes.post('/sessions', SessiontController.create)
 
 routes.get('/ongs', OngController.index);
-routes.post('/ongs', OngController.create);
 
-routes.get('/profile', ProfiletController.index);
+routes.post('/ongs', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        name: Joi.string().required().min(3),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.number().required().min(13).max(13),
+        city: Joi.string().required(),
+        uf: Joi.string().required().length(2)
+    })
+}), OngController.create);
+
+routes.get('/profile', celebrate({
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required()
+    }).unknown()
+}), ProfiletController.index);
 
 routes.post('/incidents', IncidentController.create)
-routes.get('/incidents', IncidentController.index);
-routes.delete('/incidents/:id', IncidentController.delete)
+routes.get('/incidents', celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+        page: Joi.number()
+    })
+}), IncidentController.index);
+
+routes.delete('/incidents/:id', celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+        id: Joi.number().required(),
+    })
+}),IncidentController.delete)
 
 
 
